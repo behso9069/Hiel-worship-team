9e8bda1f03ef44c81e319867e32756c8d2fbe740
behso9069 <behso9069@gmail.com>
2025-12-09 13:11:47 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from '@/App.jsx'
 import '@/index.css'
 
+// QueryClient 생성
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-) 
+  </QueryClientProvider>
+)
